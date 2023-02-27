69398e22015217b2c8228aebc0a54e7f91222b23
Stephen <tallmankaranjastevee@gmail.com>
2023-02-27 13:24:52 -0800

    0x02. C - Functions, nested loops C

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the asolute value of an integer
+ * @c: the number to be computed
+ * Return: absolute number of value or zero
+ */
+int _abs(int c)
+{
+if (c < 0)
+{
+int abs_val;
+
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+}
