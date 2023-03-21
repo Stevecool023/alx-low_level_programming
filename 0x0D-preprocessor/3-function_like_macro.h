5607d7bb88dd12b02b951cc2ca68c8460b89ed3d
Stephen <tallmankaranjastevee@gmail.com>
2023-03-20 20:03:32 -0700

    0x0D-preprocessor#

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_LIKE3_MACRO_H
+#ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(X) ((X) < (0) ? -(X) : (X))
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 
 #endif
